e104d3e737fe2ff1481cee1e549b3d2eeefe1631
cholokei <leesl0416@naver.com>
2013-08-05 00:39:18 +0900

    Fix

--- gcc.sh
+++ gcc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=$(pwd)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:$PATH
+export PATH=$HOME/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin:$PATH
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
