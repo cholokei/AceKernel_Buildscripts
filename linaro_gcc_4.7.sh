cd6bef009a22b8e73e9f71e155e01d40f3415f58
cholokei <leesl0416@naver.com>
2013-10-10 00:50:38 +0900

    Update linaro gcc setup script

--- linaro_gcc_4.7.sh
+++ linaro_gcc_4.7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=$HOME/linaro-toolchain/android-toolchain-eabi/bin:$PATH
+export PATH=$HOME/linaro-toolchain/4.7/android-toolchain-eabi/bin:$PATH
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-eabi-
---
+++ linaro_gcc_4.8.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=$HOME/linaro-toolchain/4.8/android-toolchain-eabi/bin:$PATH
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-eabi-
